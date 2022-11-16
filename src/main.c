b5335b9274e7e51153995413010257551378db51
Michael Baumgarten <mbaum0@github.com>
2022-11-16 12:53:32 +0100

    argument to specify game mode

--- src/main.c
+++ src/main.c
@@ -6,9 +6,22 @@
 int main(int argc, char **argv) {
     (void)argc;
     (void)argv;
+    
+    if (argc < 2){
+        log_warn("Game mode not specified [single/multi], defaulting to single player.");
+    }
+    
+    GameMode mode = SINGLE_PLAYER;
+    if (strncmp(argv[1], "multi", 5) == 0){
+        mode = MULTI_PLAYER;
+        log_info("Starting a new multi player game");
+    } else {
+        log_info("Started a new single player game");
+    }
 
+    GameRunner* game = createGame(mode);
     srand(time(0));
-    GameRunner *game = createGame(MULTI_PLAYER);
+    
     runGame(game);
     destroyGame(game);
 }
